17f1f3ff2e94aad0e6444fbdda2e18be11d17e5d
Lin Lin Hlaing <linlinhlaing@Lins-Air.attlocal.net>
2024-03-17 00:17:36 -0400

    added for Marvel the model, representing a Marvel team member:

---
+++ Marvel.py
@@ -0,0 +1,24 @@
+marvel_members = []
+class Marvel:
+
+    __name = ''
+    __height = 0
+    __weight = 0
+    __games_played = 0
+    def setName(self,name):
+        self.__name = name
+    def getName(self):
+        return self.__name
+    def setHeight(self,height):
+        self.__height = height
+    def getHeight(self):
+        return self.__height
+    def setWeight(self,weight):
+        self.__weight = weight
+    def getWeight(self):
+        return self.__weight
+    def setGamesPlayed(self,games_played):
+        self.__games_played = games_played
+    def getGamesPlayed(self):
+        return self.__games_played
+
